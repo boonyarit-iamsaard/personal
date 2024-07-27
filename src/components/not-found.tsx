6406bd7d587e563df140d585917cba3c16ab762f
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2024-07-27 13:36:29 +0700

    build: add not found page

---
+++ src/components/not-found.tsx
@@ -0,0 +1,7 @@
+export function NotFound() {
+  return (
+    <section className="container flex min-h-screen flex-col items-center justify-center p-24">
+      <h1>404 | This page could not be found.</h1>
+    </section>
+  );
+}
